5e59c75a3e42e711ca37e060d8e1414d486e93fa
r3yz0n <reyzon.thapa2020@gmail.com>
2024-08-14 00:01:02 +0545

    fix:small change

--- react-101/learning-hooks/src/App.jsx
+++ react-101/learning-hooks/src/App.jsx
@@ -1,19 +1,6 @@
 import React, { useState } from "react";
 
 const App = () => {
-  // state is a variable that is used to store data that can be used in the component
-  // state is used to store data that can change over time
-  // state is used to store data that can be used in the component
-  // virtual DOM is used to update the UI when the state changes
-  // real dom vs virtual dom
-  // real dom is the actual dom that is rendered on the browser
-  // virtual dom is a copy of the real dom that is stored in memory
-  // virtual dom is used to update the real dom when the state changes  (diffing algorithm)
-  //useState is a hook that allows you to have state variables in functional components
-  //useState returns an array with two elements
-  //first element is the state variable
-  //second element is the function to update the state variable
-
   const [count, setCount] = useState(0);
   const [email, setEmail] = useState("");
 
